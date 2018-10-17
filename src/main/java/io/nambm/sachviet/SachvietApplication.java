49287dc14979bfe7bc9a6a96cc700eb5286fe488
NAMBM <nambach2@Gmail.com>
2018-10-17 07:42:24 +0700

    Initial commit

---
+++ src/main/java/io/nambm/sachviet/SachvietApplication.java
@@ -0,0 +1,12 @@
+package io.nambm.sachviet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SachvietApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SachvietApplication.class, args);
+    }
+}
